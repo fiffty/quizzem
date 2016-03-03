04c03b30878cae6d5ab491357e98eb3961fe348a
fiffty <jyoo@Jins-MacBook-Air.local>
2016-03-03 15:26:41 -0500

    fixing goToStep()

--- quizzem/quizzem.js
+++ quizzem/quizzem.js
@@ -1,1 +1,267 @@
-"use strict";function qzm(a,b){function c(c,d,e,f){function g(){c.showError=!1;var b=c.code+"\n"+c.testCode,d=quizzemWebWorker.toString();d=d.substring(d.indexOf("{")+1,d.lastIndexOf("}"));var e=new Blob([d],{type:"application/javascript"}),f=new Worker(URL.createObjectURL(e));f.postMessage(b);var g=a.defer();f.onmessage=function(a){g.resolve(a.data),f.terminate()},g.promise.then(function(a){(c.errorInUserCode=a.error[0])?(c.showError=!0,c.qzmOnCheck({passed:!1})):(c.currentStep==c.maxStep&&c.maxStep++,c.currentStep<c.inputTests.length-1?(i(k,++c.currentStep),c.qzmOnCheck({passed:!1})):c.qzmOnCheck({passed:!0}))})}function h(a){}function i(a,b){c.code=c.inputTests[b].startingCode||c.code,a.setValue(c.code),c.testCode=c.inputTests[b].testCode,c.language=c.inputTests[b].language,c.instructions=c.inputTests[b].instructions,c.currentStep=b}function j(){if(k){var a=k.getWrapperElement();a.parentNode.removeChild(a),k=null}var b=c.inputOptions.codemirrorOptions;b.mode=c.inputTests[0].language.toLowerCase(),k=new CodeMirror(document.getElementById("qzm-codemirror"),b),k.on("change",function(a){c.code=a.getValue()})}if(angular.isUndefined(CodeMirror))throw new Error("Quizzem needs CodeMirror to work.");c.checkWork=g,c.codemirrorLoaded=h,c.goToStep=i,c.currentStep=0,c.maxStep=0,c.showError=!1;var k;c.$watch(function(){return c.inputTests},function(a){a&&b(function(){j(),i(k,0)})})}return{templateUrl:templatePath,restrict:"EA",scope:{inputOptions:"=qzmOptions",inputTests:"=qzmTests",inputRefreshState:"=qzmRefreshState",qzmOnCheck:"&"},link:c}}function quizzemWebWorker(){!function(){function a(a){var b=this;b.did={console:{log:[]},angular:{module:[],moduleArray:[],controller:[],controllerFunc:[],service:[],serviceFunc:[],filter:[],filterFunc:[]}},b.console={log:function(a){b.did.console.log.push(a)}};var c=function(b,c,d){b&&c?"string"!=typeof b?a.error.push("The first parameter for "+d+" must be a string."):"function"!=typeof c&&a.error.push("The second parameter for "+d+" must be a function."):a.error.push(d+" method needs two parameters when initiating an app.")};b.angular={module:function(c,d){return c&&d?"string"!=typeof c?a.error.push("The first parameter for angular.module() must be a string."):"object"!=typeof d&&a.error.push("The second parameter for angular.module() must be an array."):a.error.push("angular.module() method needs two parameters when initiating an app."),a.error[0]||(b.did.angular.module.push(c),b.did.angular.moduleArray.push(d)),b.angular.module=b.angular.moduleGetter,b.angular},moduleGetter:function(c){return"string"!=typeof c&&a.error.push("The parameter for angular.module() must be a string."),a.error[0]||b.did.angular.module.push(c),b.angular},controller:function(d,e){return c(d,e,".controller()"),a.error[0]||(b.did.angular.controller.push(d),b.did.angular.controllerFunc.push(e)),b.angular},service:function(d,e){return c(d,e,".service()"),a.error[0]||(b.did.angular.service.push(d),b.did.angular.serviceFunc.push(e)),b.angular},filter:function(d,e){return c(d,e,".filter()"),a.error||(b.did.angular.filter.push(d),b.did.angular.filterFunc.push(e)),b.angular}},b.process={argv:["node","app.js","1","2","hello","4"]}}var b=postMessage,c=addEventListener;!function(a){var b=a,c=["Object","Function","Infinity","NaN","undefined","Array","Boolean","Number","String","Symbol","Map","Math","Set","isNaN","caches","TEMPORARY","PERSISTENT"];do Object.getOwnPropertyNames(b).forEach(function(a){-1===c.indexOf(a)&&delete b[a]}),b=Object.getPrototypeOf(b);while(b!==Object.prototype)}(this),c("message",function(c){var d={error:[]};a(d);var e=new Function(c.data);try{var f=e();0==f[0]?d.error.push(f[1]):d.success=f[1]}catch(g){d.error.push(g.toString())}b(d)})}()}var scripts=document.getElementsByTagName("script"),currentScriptPath=scripts[scripts.length-1].src;if("min.js"==currentScriptPath.slice(-6))var templatePath=currentScriptPath.replace("quizzem.min.js","quizzem.html");else var templatePath=currentScriptPath.replace("quizzem.js","quizzem.html");angular.module("quizzem",[]).directive("qzm",["$q","$timeout",qzm]);
+'use strict';
+
+var scripts = document.getElementsByTagName("script");
+var currentScriptPath = scripts[scripts.length-1].src;
+if (currentScriptPath.slice(-6) == 'min.js') {
+	var templatePath = currentScriptPath.replace('quizzem.min.js', 'quizzem.html');
+} else {
+	var templatePath = currentScriptPath.replace('quizzem.js', 'quizzem.html');
+}
+
+angular.module('quizzem', [])
+.directive('qzm', ['$q', '$timeout', qzm]);
+
+
+// Angular Directive
+function qzm($q, $timeout) {
+	return {
+		templateUrl: templatePath,
+		restrict: 'EA',
+		scope: {
+			inputOptions: '=qzmOptions',
+			inputTests: '=qzmTests',
+			inputRefreshState: '=qzmRefreshState',
+			qzmOnCheck: '&'
+		},
+		link: QzmLink,
+	};
+
+	function QzmLink(scope, elem, attr, ngModel) {
+		// Require CodeMirror
+		if (angular.isUndefined(CodeMirror)) {
+			throw new Error('Quizzem needs CodeMirror to work.');
+		}
+
+
+		// METHODS
+		scope.checkWork 		= checkWork; // method that checks user submitted work
+		scope.codemirrorLoaded 	= codemirrorLoaded; // callback method for codemirror load
+		scope.goToStep 			= goToStep; // navigate between quiz stages
+
+		// INITIAL SETUP
+		// setup meta info for quiz steps
+		scope.currentStep 		= 0; // track current step
+		scope.maxStep			= 0; // track progress
+		scope.showError 		= false;
+		// create a new codemirror instance when
+		var codemirror;
+		// scope.inputTests receive a value
+		scope.$watch(function() {
+			return scope.inputTests;
+		}, function(newVal) {
+			if (newVal) { // make sure newVal is not empty
+				$timeout(function() {
+					renderCodemirror();
+					goToStep(codemirror, 0);
+				});
+			}
+		});
+
+		// HOISTED METHODS
+		// method that checks user submitted work
+		function checkWork() {
+			scope.showError = false;
+			var stringifiedFunc = scope.code + '\n' + scope.testCode;
+
+			// create web worker
+			var workerCode = quizzemWebWorker.toString();
+			workerCode = workerCode.substring(workerCode.indexOf("{")+1, workerCode.lastIndexOf("}"));
+			var blob = new Blob([workerCode], {type:'application/javascript'});
+			var worker = new Worker(URL.createObjectURL(blob));
+			
+			// send message to worker
+			worker.postMessage(stringifiedFunc);
+
+			// handle message from worker
+			var deferred = $q.defer();
+			worker.onmessage = function(e) {
+				deferred.resolve(e.data);
+				worker.terminate();
+			};
+			deferred.promise.then(function(data) {
+				if (scope.errorInUserCode = data.error[0]) {
+					scope.showError = true;
+					// pass out result of code check
+					scope.qzmOnCheck({passed: false});
+				} else {
+					// pass out result of code check
+					if (scope.currentStep == scope.maxStep) scope.maxStep++;
+					if (scope.currentStep < scope.inputTests.length - 1) {
+						goToStep(codemirror, ++scope.currentStep);
+						scope.qzmOnCheck({passed: false});
+					} else {
+						scope.qzmOnCheck({passed: true});
+					}
+				}
+			});
+		}
+		// callback method for codemirror load
+		function codemirrorLoaded(editor) {}
+		// navigate between quiz stages
+		function goToStep(codemirror, index, userClick) { // userClick is true if this method is called from a user click
+			if (userClick && scope.currentStep == scope.maxStep && scope.currentStep == index) { return checkWork();};
+			scope.code 			= scope.inputTests[index].startingCode || scope.code;
+			codemirror.setValue(scope.code); // set codemirror's value to scope varialbe
+			scope.testCode 		= scope.inputTests[index].testCode;
+			scope.language 		= scope.inputTests[index].language;
+			scope.instructions 	= scope.inputTests[index].instructions;
+			scope.currentStep 	= index;			
+		}
+		// render codemirror
+		function renderCodemirror() {
+			if (codemirror) {
+				var cmElem = codemirror.getWrapperElement();
+				cmElem.parentNode.removeChild(cmElem);
+				codemirror = null;				
+			}
+			var opts = scope.inputOptions.codemirrorOptions;
+			opts.mode = scope.inputTests[0].language.toLowerCase();
+			codemirror = new CodeMirror(document.getElementById('qzm-codemirror'), opts);
+			scope.codemirror = codemirror;
+			// stream codemirror changes to scope variable
+			codemirror.on('change', function(instance) {
+				scope.code = instance.getValue();
+			});				
+			// streaming scope variable changes to codemirror happens in goToStep()			
+		}
+	}
+};
+
+// Web worker
+function quizzemWebWorker() {
+	(function () {
+	    // save methods in local variable before stripping away properties
+	    var _postMessage = postMessage;
+	    var _addEventListener = addEventListener;
+
+	    // anonymous function to strip away properties that aren't needed
+	    (function (obj) {
+	        'use strict';
+
+	        var current = obj; // currentlt, current => DedicatedWorkerGlobalScope 
+	        var keepProperties = [
+	            // required by JavaScript
+	            'Object', 'Function', 'Infinity', 'NaN', 'undefined',
+	            // other stuff we want to keep
+	            'Array', 'Boolean', 'Number', 'String', 'Symbol', 'Map', 'Math', 'Set', 'isNaN',
+	            // stuff that cannot be deleted
+	            'caches', 'TEMPORARY', 'PERSISTENT'
+	        ];
+
+	        do {
+	            Object.getOwnPropertyNames(current).forEach(function (name) {
+	                if (keepProperties.indexOf(name) === -1) {
+	                    delete current[name];
+	                }
+	            });
+
+	            // get higher scope and strip away properties again
+	            current = Object.getPrototypeOf(current);
+	        } while (current !== Object.prototype); // until we get to Object() 
+	    })(this);
+
+	    // listen to message being sent in to worker
+	    _addEventListener('message', function (e) {
+	        var msg = {
+	            error: [],
+	        };
+	        // initialize test enviroment
+	        testEnv(msg);
+	        // construct the function inside the worker whose scope has limited properties
+	        var f = new Function(e.data);
+	        
+	        // run the function inside the worker, and send its returned value as message
+	        try {
+	            var obj = f();
+	            if (obj[0] == false) {
+	                msg.error.push(obj[1]);
+	            } else {
+	                msg.success = obj[1];
+	            }
+	        } catch(err) {
+	            msg.error.push(err.toString());
+	        }
+	        _postMessage(msg);
+	    });
+
+	    function testEnv(msg) {
+	        var self = this;
+
+	        // object that logs psuedo method calls
+	        // visible to module javascript objects
+	        self.did = {
+	            console: {
+	                log: [],
+	            },
+	            angular: {
+	                module: [],
+	                moduleArray: [],
+	                controller: [],
+	                controllerFunc: [],
+	                service: [],
+	                serviceFunc: [],
+	                filter: [],
+	                filterFunc: [],
+	            }
+	        }
+
+	        // PSUEDO OBJECTS
+
+	        // console
+	        self.console = {
+	            log: function(a) {
+	                self.did.console.log.push(a);
+	            }
+	        }
+
+	        // angular
+	        var errorMessagesSetters = function(name, func, key) {
+	            if (!(name && func)) {
+	                msg.error.push(key + ' method needs two parameters when initiating an app.');
+	            } else if (typeof(name) != 'string') {
+	                msg.error.push('The first parameter for ' + key + ' must be a string.');
+	            } else if (typeof(func) != 'function') {
+	                msg.error.push('The second parameter for ' + key + ' must be a function.');
+	            }         
+	        }
+	        self.angular = {
+	            module: function(name, array) {
+	                if (!(name && array)) {
+	                    msg.error.push('angular.module() method needs two parameters when initiating an app.');
+	                } else if (typeof(name) != 'string') {
+	                    msg.error.push('The first parameter for angular.module() must be a string.');
+	                } else if (typeof(array) != 'object') {
+	                    msg.error.push('The second parameter for angular.module() must be an array.');
+	                }
+	                if (!msg.error[0]) {self.did.angular.module.push(name);self.did.angular.moduleArray.push(array);}
+	                self.angular.module = self.angular.moduleGetter;
+	                return self.angular;
+	            },
+	            moduleGetter: function(name) {
+	                if (typeof(name) != 'string') msg.error.push('The parameter for angular.module() must be a string.');
+	                if (!msg.error[0]) {self.did.angular.module.push(name);}
+	                return self.angular;
+	            },
+	            controller: function(name, func) {
+	                errorMessagesSetters(name, func, '.controller()');
+	                if (!msg.error[0]) {self.did.angular.controller.push(name);self.did.angular.controllerFunc.push(func);} 
+	                return self.angular;              
+	            },
+	            service: function(name, func) {
+	                errorMessagesSetters(name, func, '.service()');
+	                if (!msg.error[0]) {self.did.angular.service.push(name);self.did.angular.serviceFunc.push(func)}
+	                return self.angular;                 
+	            },
+	            filter: function(name, func) {
+	                errorMessagesSetters(name, func, '.filter()');
+	                if (!msg.error) {self.did.angular.filter.push(name);self.did.angular.filterFunc.push(func);}
+	                return self.angular;
+	            }
+	        }
+	        //node
+	        self.process = {
+	            argv: ['node','app.js','1','2','hello', '4'],
+	        } 
+	    }
+	})();
+}
